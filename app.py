9e4f0f53beddb39bd0141fd76d7da8f4bb406580
this-is-singh19 <thisissingh19@gmail.com>
2023-07-14 23:15:55 +0530

    UI improvement

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
 import streamlit as st
 import pandas as pd
-from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
-import plotly.figure_factory as ff
-from sklearn.metrics import accuracy_score
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
 import seaborn as sns
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('diabetes.csv')
 
 # HEADINGS
-st.title('Diabetes Checkup')
-st.sidebar.header('Patient Data')
-st.subheader('Training Data Stats')
-st.write(df.describe())
+st.title('SugarWatch - Diabetic Prediction Web App')
+st.sidebar.header('Patient Input Parameters')
+st.subheader('Training Data Set Information')
+st.dataframe(df.describe())
 
 # X AND Y DATA
-x = df.drop(['Outcome'], axis = 1)
-y = df.iloc[:, -1]
-x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.2, random_state = 0)
+x = df.drop(['Outcome'], axis=1)
+y = df['Outcome']
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
 # FUNCTION
 def user_report():
@@ -48,103 +46,102 @@ def user_report():
 # PATIENT DATA
 user_data = user_report()
 st.subheader('Patient Data')
-st.write(user_data)
+st.dataframe(user_data)
 
 # MODEL
-rf  = RandomForestClassifier()
-rf.fit(x_train, y_train)
-user_result = rf.predict(user_data)
+gbm = GradientBoostingClassifier()
+gbm.fit(x_train, y_train)
+user_result = gbm.predict(user_data)
 
-# VISUALISATIONS
-st.title('Visualised Patient Report')
+# VISUALIZATIONS
+st.title('Visualized Patient Report')
 
 # COLOR FUNCTION
-if user_result[0]==0:
-  color = 'blue'
+if user_result[0] == 0:
+    color = 'blue'
 else:
-  color = 'red'
+    color = 'red'
 
 # Age vs Pregnancies
 st.header('Pregnancy count Graph (Others vs Yours)')
-fig_preg = plt.figure()
-ax1 = sns.scatterplot(x = 'Age', y = 'Pregnancies', data = df, hue = 'Outcome', palette = 'Greens')
-ax2 = sns.scatterplot(x=user_data['Age'], y=user_data['Pregnancies'], s=150, color=color)
-
-plt.xticks(np.arange(10,100,5))
-plt.yticks(np.arange(0,20,2))
+fig_preg, ax1 = plt.subplots(figsize=(8, 6))
+sns.scatterplot(x='Age', y='Pregnancies', data=df, hue='Outcome', palette='coolwarm', ax=ax1)
+sns.scatterplot(x=user_data['Age'], y=user_data['Pregnancies'], s=150, color=color, marker='s', ax=ax1)
+ax1.grid(True)
+plt.xticks(np.arange(10, 100, 5))
+plt.yticks(np.arange(0, 20, 2))
 plt.title('0 - Healthy & 1 - Unhealthy')
 st.pyplot(fig_preg)
 
 # Age vs Glucose
 st.header('Glucose Value Graph (Others vs Yours)')
-fig_glucose = plt.figure()
-ax3 = sns.scatterplot(x = 'Age', y = 'Glucose', data = df, hue = 'Outcome' , palette='magma')
-#ax4 = sns.scatterplot(x=user_data['age'], y=user_data['glucose'], s=150, color=color)
-ax4 = sns.scatterplot(x=user_data['Age'], y=user_data['Glucose'], s=150, color=color)
-plt.xticks(np.arange(10,100,5))
-plt.yticks(np.arange(0,220,10))
+fig_glucose, ax2 = plt.subplots(figsize=(8, 6))
+sns.scatterplot(x='Age', y='Glucose', data=df, hue='Outcome', palette='viridis', ax=ax2)
+sns.scatterplot(x=user_data['Age'], y=user_data['Glucose'], s=150, color=color, marker='s', ax=ax2)
+ax2.grid(True)
+plt.xticks(np.arange(10, 100, 5))
+plt.yticks(np.arange(0, 220, 10))
 plt.title('0 - Healthy & 1 - Unhealthy')
 st.pyplot(fig_glucose)
 
 # Age vs Bp
 st.header('Blood Pressure Value Graph (Others vs Yours)')
-fig_bp = plt.figure()
-ax5 = sns.scatterplot(x = 'Age', y = 'BloodPressure', data = df, hue = 'Outcome', palette='Reds')
-#ax6 = sns.scatterplot(x = user_data['age'], y = user_data['bp'], s = 150, color = color)
-#ax6 = sns.scatterplot(x=user_data['Age'], y=user_data['bp'], s=150, color=color)
-ax6 = sns.scatterplot(x=user_data['Age'], y=user_data['BloodPressure'], s=150, color=color)
-plt.xticks(np.arange(10,100,5))
-plt.yticks(np.arange(0,130,10))
+fig_bp, ax3 = plt.subplots(figsize=(8, 6))
+sns.scatterplot(x='Age', y='BloodPressure', data=df, hue='Outcome', palette='rocket', ax=ax3)
+sns.scatterplot(x=user_data['Age'], y=user_data['BloodPressure'], s=150, color=color, marker='s', ax=ax3)
+ax3.grid(True)
+plt.xticks(np.arange(10, 100, 5))
+plt.yticks(np.arange(0, 130, 10))
 plt.title('0 - Healthy & 1 - Unhealthy')
 st.pyplot(fig_bp)
 
 # Age vs St
 st.header('Skin Thickness Value Graph (Others vs Yours)')
-fig_st = plt.figure()
-ax7 = sns.scatterplot(x = 'Age', y = 'SkinThickness', data = df, hue = 'Outcome', palette='Blues')
-ax8 = sns.scatterplot(x = user_data['Age'], y = user_data['SkinThickness'], s = 150, color = color)
-plt.xticks(np.arange(10,100,5))
-plt.yticks(np.arange(0,110,10))
+fig_st, ax4 = plt.subplots(figsize=(8, 6))
+sns.scatterplot(x='Age', y='SkinThickness', data=df, hue='Outcome', palette='Blues', ax=ax4)
+sns.scatterplot(x=user_data['Age'], y=user_data['SkinThickness'], s=150, color=color, marker='s', ax=ax4)
+ax4.grid(True)
+plt.xticks(np.arange(10, 100, 5))
+plt.yticks(np.arange(0, 110, 10))
 plt.title('0 - Healthy & 1 - Unhealthy')
 st.pyplot(fig_st)
 
 # Age vs Insulin
 st.header('Insulin Value Graph (Others vs Yours)')
-fig_i = plt.figure()
-ax9 = sns.scatterplot(x = 'Age', y = 'Insulin', data = df, hue = 'Outcome', palette='rocket')
-ax10 = sns.scatterplot(x = user_data['Age'], y = user_data['Insulin'], s = 150, color = color)
-plt.xticks(np.arange(10,100,5))
-plt.yticks(np.arange(0,900,50))
+fig_i, ax5 = plt.subplots(figsize=(8, 6))
+sns.scatterplot(x='Age', y='Insulin', data=df, hue='Outcome', palette='rocket', ax=ax5)
+sns.scatterplot(x=user_data['Age'], y=user_data['Insulin'], s=150, color=color, marker='s', ax=ax5)
+ax5.grid(True)
+plt.xticks(np.arange(10, 100, 5))
+plt.yticks(np.arange(0, 900, 50))
 plt.title('0 - Healthy & 1 - Unhealthy')
 st.pyplot(fig_i)
 
 # Age vs BMI
 st.header('BMI Value Graph (Others vs Yours)')
-fig_bmi = plt.figure()
-ax11 = sns.scatterplot(x = 'Age', y = 'BMI', data = df, hue = 'Outcome', palette='rainbow')
-ax12 = sns.scatterplot(x = user_data['Age'], y = user_data['BMI'], s = 150, color = color)
-plt.xticks(np.arange(10,100,5))
-plt.yticks(np.arange(0,70,5))
+fig_bmi, ax6 = plt.subplots(figsize=(8, 6))
+sns.scatterplot(x='Age', y='BMI', data=df, hue='Outcome', palette='rainbow', ax=ax6)
+sns.scatterplot(x=user_data['Age'], y=user_data['BMI'], s=150, color=color, marker='s', ax=ax6)
+ax6.grid(True)
+plt.xticks(np.arange(10, 100, 5))
+plt.yticks(np.arange(0, 70, 5))
 plt.title('0 - Healthy & 1 - Unhealthy')
 st.pyplot(fig_bmi)
 
 # Age vs Dpf
 st.header('DPF Value Graph (Others vs Yours)')
-fig_dpf = plt.figure()
-ax13 = sns.scatterplot(x = 'Age', y = 'DiabetesPedigreeFunction', data = df, hue = 'Outcome', palette='YlOrBr')
-ax14 = sns.scatterplot(x = user_data['Age'], y = user_data['DiabetesPedigreeFunction'], s = 150, color = color)
-plt.xticks(np.arange(10,100,5))
-plt.yticks(np.arange(0,3,0.2))
+fig_dpf, ax7 = plt.subplots(figsize=(8, 6))
+sns.scatterplot(x='Age', y='DiabetesPedigreeFunction', data=df, hue='Outcome', palette='YlOrBr', ax=ax7)
+sns.scatterplot(x=user_data['Age'], y=user_data['DiabetesPedigreeFunction'], s=150, color=color, marker='s', ax=ax7)
+ax7.grid(True)
+plt.xticks(np.arange(10, 100, 5))
+plt.yticks(np.arange(0, 3, 0.2))
 plt.title('0 - Healthy & 1 - Unhealthy')
 st.pyplot(fig_dpf)
 
 # OUTPUT
-st.subheader('Your Report: ')
-output=''
-if user_result[0]==0:
-  output = 'You are not Diabetic'
-else:
-  output = 'You are Diabetic'
+st.subheader('Your Report:')
+output = 'You are not Diabetic' if user_result[0] == 0 else 'You are Diabetic'
 st.title(output)
-st.subheader('Accuracy: ')
-st.write(str(accuracy_score(y_test, rf.predict(x_test))*100)+'%')
+st.subheader('Accuracy:')
+st.header(f"{round(accuracy_score(y_test, gbm.predict(x_test))*100, 2)}%")
